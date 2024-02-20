9fc291485096b61c8ad690003960dd4a3e901755
polinar08 <apolinariy2000@gmail.com>
2024-02-20 21:28:51 +1000

    Updated files

--- utils/Category.py
+++ utils/Category.py
@@ -1,11 +1,11 @@
 class Category:
-    """Class representing a category of products."""
-    total_categories = 0
-    unique_products = 0
+    """Класс категории."""
+    total_categories = 0  # атрибут для отслеживания общего количества категорий
+    unique_products = 0  # атрибут для отслеживания общего количества уникальных продуктов
 
     def __init__(self, name, description, products):
         self.name = name  # название категории
         self.description = description  # описание категории
         self.products = products  # товары в категории
-        Category.total_categories += 1
-        Category.unique_products += len(products)
+        Category.total_categories += 1  # увеличиваем общее количество категорий
+        Category.unique_products += len(products)  # увеличиваем общее количество уникальных продуктов
--- utils/Main.py
+++ utils/Main.py
@@ -1,3 +1,51 @@
 from Category import Category
 from Product import Product
+import json
 
+
+def load_data():
+    # Загружаем данные из файла products.json
+    with open("products.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
+def main():
+    data = load_data()  # Загрузка данных
+    categories = []
+
+    # Обработка данных и создание экземпляров классов Category и Product
+    for category_data in data:
+        products = []
+        for product_data in category_data["products"]:
+            product = Product(
+                product_data["name"],
+                product_data["description"],
+                product_data["price"],
+                product_data["quantity"]
+            )
+            products.append(product)
+
+        category = Category(
+            category_data["name"],
+            category_data["description"],
+            products
+        )
+        categories.append(category)
+
+    # Вывод информации о категориях и продуктах
+    for category in categories:
+        print(f"Категория: {category.name}")
+        print(f"Описание: {category.description}")
+        print("Продукты:")
+        for product in category.products:
+            print(f"\tПродукт: {product.name}")
+            print(f"\tОписание: {product.description}")
+            print(f"\tЦена: {product.price}")
+            print(f"\tКоличество: {product.quantity_available}")
+            print()
+        print()
+
+
+if __name__ == "__main__":
+    main()
