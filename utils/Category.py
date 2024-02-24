0a9f0a5b9fd91b150fb77a50af4f5823daef8cd4
polinar08 <apolinariy2000@gmail.com>
2024-02-24 22:07:09 +1000

    zadanie 2, cotegories

--- utils/Category.py
+++ utils/Category.py
@@ -1,16 +1,23 @@
 class Category:
-    """Class representing a category of products."""
+    """Класс категории."""
+
+    total_categories = 0  # атрибут для отслеживания общего количества категорий
+    unique_products = 0  # атрибут для отслеживания общего количества уникальных продуктов
 
     def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        self.__products = []  # Приватный атрибут для хранения товаров
+        self.name = name  # название категории
+        self.description = description  # описание категории
+        self.__products = []  # приватный атрибут для хранения товаров
+        Category.total_categories += 1  # увеличиваем общее количество категорий
 
     def add_product(self, product):
         """Метод для добавления товара в категорию."""
         self.__products.append(product)
 
     @property
-    def products(self):
-        """Геттер для получения списка товаров."""
-        return self.__products
+    def formatted_products(self):
+        """Геттер для вывода списка товаров в формате."""
+        formatted_list = []
+        for product in self.__products:
+            formatted_list.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity_available} шт.")
+        return formatted_list
