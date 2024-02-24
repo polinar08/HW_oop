c413fafe42c89d7a9ae69dadd12b98ace9d3b950
polinar08 <apolinariy2000@gmail.com>
2024-02-24 22:17:48 +1000

    zadanie 4, products

--- utils/Product.py
+++ utils/Product.py
@@ -4,18 +4,24 @@ class Product:
     def __init__(self, name, description, price, quantity_available):
         self.name = name  # название продукта
         self.description = description  # описание продукта
-        self.price = price  # цена продукта
+        self.__price = price  # цена продукта (приватный атрибут)
         self.quantity_available = quantity_available  # количество товара в наличии
 
-    @classmethod
-    def create(cls, name, description, price, quantity_available, product_list=None):
-        """Метод для создания нового товара и проверки наличия дубликата."""
-        if product_list:
-            for existing_product in product_list:
-                if existing_product.name == name:
-                    # Если товар уже существует, обновляем количество в наличии и цену
-                    existing_product.quantity_available += quantity_available
-                    existing_product.price = max(existing_product.price, price)
-                    return existing_product
-        # Если товар не найден или список товаров пуст, создаем новый объект товара
-        return cls(name, description, price, quantity_available)
+    @property
+    def price(self):
+        """Геттер для получения цены товара."""
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        """Сеттер для установки цены товара."""
+        if value > 0:
+            # Проверяем, если цена понижается, запрашиваем подтверждение пользователя
+            if value < self.__price:
+                confirm = input("Вы уверены, что хотите понизить цену? (y/n): ")
+                if confirm.lower() != 'y':
+                    print("Действие отменено.")
+                    return
+            self.__price = value
+        else:
+            print("Цена введена некорректно.")
